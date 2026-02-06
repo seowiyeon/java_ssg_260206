4355b1b3d972622d5983ebe6a4e4332ced7ebf6c
seowiyeon <weyeon06@naver.com>
2026-02-06 15:26:28 +0900

    작업1, 스캐너 입력받기

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import java.util.Scanner;
+
+public class Main {
+    static void main() {
+        System.out.println("== 프로그램 시작 ==");
+        Scanner sc = new Scanner(System.in);
+
+        IO.print("명령어) ");
+        String cmd = sc.nextLine();
+        IO.println(String.format("입력된 명령어 : %s\n", cmd));
 
 
-void main() {
-    System.out.println("안녕");
+        System.out.println("== 프로그램 끝 ==");
+        sc.close();
     }
+}
