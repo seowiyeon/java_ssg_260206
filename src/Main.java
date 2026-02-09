bb97984d7f3c7010d3b227cb698705dad8f24505
seowiyeon <weyeon06@naver.com>
2026-02-09 13:55:17 +0900

    초기 작업

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    System.out.println("안녕");
 }
